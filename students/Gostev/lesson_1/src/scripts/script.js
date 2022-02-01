580c79ea070ea6206cd493619b8f4ac844c516be
nekktos <84871775+nekktos@users.noreply.github.com>
2022-02-01 22:44:39 +0300

    Add files via upload
    Первый урок, простой калькулятор

---
+++ students/Gostev/lesson_1/src/scripts/script.js
@@ -0,0 +1,21 @@
+function plus() {
+    let a = +prompt('Enter A');
+    let b = +prompt('Enter B');
+    console.log(a + b);
+  };
+function minus() {
+    let a = +prompt('Enter A');
+    let b = +prompt('Enter B');
+    console.log(a - b);
+  }
+function multiply() {
+    let a = +prompt('Enter A');
+    let b = +prompt('Enter B');
+    console.log(a * b);
+  };
+function dvide() {
+    let a = +prompt('Enter A');
+    let b = +prompt('Enter B');
+    console.log(a / b);
+  }
+  
