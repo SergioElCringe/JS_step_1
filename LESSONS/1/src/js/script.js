2640bf73ef8fabb707a6d2d1ac4e68e56e75b394
kellolo <87kell@gmail.com>
2022-02-01 20:17:54 +0200

    1st lesson

---
+++ LESSONS/1/src/js/script.js
@@ -0,0 +1,71 @@
+// console.log('Hello file.js')
+
+// 1 line commentary
+
+/*
+  several
+  lines
+  commentary
+*/
+
+// several
+// lines
+// commentary
+// by
+// one
+// line
+
+
+// Типы данных в ЖС
+
+// по категории
+// 1) Примитивные
+// 2) Объекты (ссылочные)
+
+// По типу
+
+// 1. Неопределенный тип - undefined
+// 2. НИЧТО - null
+// 3. Число - Number
+// 4. Строка - String
+// 5. Логический - Boolean
+// 6. Объект - Object
+// ==========================
+// 7. Символьный - Symbol
+// 8. Большие числа - BigInt
+
+
+// var a = 100;
+// var b = 200;
+// var a, b;
+// a = 100;
+// b = 500;
+
+// console.log(a + b);
+
+// var a = 100;
+// var b = 100;
+
+// let a = 100;
+// let b = 100;
+
+// let a = 100;
+// let b = 100;
+
+// console.log(a + b);
+
+// console.log('То, что я хочу видеть в консоли, в кач-ве результата');
+// let mult = 2 * 5;
+// console.log('Результат умножения 2 и 5 = ' + mult);
+
+// alert('Hello User'); - Фу таким быть
+
+//HTML cebab-case
+//JS camelCase
+
+// let a = +prompt('enter A');
+// let b = +prompt('enter B');
+// let sum = a + b;
+// console.log(sum);
+// console.log(typeof sum);
+
