6f5fa55ab193dca40b10e343cfe863ad101f6b47
DenisKungurov <denkungurow@yandex.ru>
2022-02-02 21:03:21 +0400

    Hw_1

--- students/kungurov_hw_1/script.js
+++ students/kungurov_hw_1/script.js
@@ -1,20 +1,20 @@
 function plus() {
-    var a = Number(prompt());
-    var b = Number(prompt());
-    console.log(a + b)
+    var a = Number(prompt('Введите первое число'));
+    var b = Number(prompt('Введите второе чило'));
+    console.log(a + b);
 }
 function minus() {
-    var a = Number(prompt());
-    var b = Number(prompt());
-    console.log(a - b)
+    var a = Number(prompt('Введите первое число'));
+    var b = Number(prompt('Введите второе чило'));
+    console.log(a - b);
 }
 function division() {
-    var a = Number(prompt());
-    var b = Number(prompt());
-    console.log(a / b)
+    var a = Number(prompt('Введите первое число'));
+    var b = Number(prompt('Введите второе чило'));
+    console.log(a / b);
 }
 function multiply() {
-    var a = Number(prompt());
-    var b = Number(prompt());
-    console.log(a * b)
+    var a = Number(prompt('Введите первое число'));
+    var b = Number(prompt('Введите второе чило'));
+    console.log(a * b);
 }
