a4ae83939bc5d288f30c46cd835d4065c3b16c63
DenisKungurov <denkungurow@yandex.ru>
2022-02-02 20:57:23 +0400

    Kungurov_hw_1

--- students/kungurov_hw_1/script.js
+++ students/kungurov_hw_1/script.js
@@ -1,20 +1,20 @@
 function plus() {
     var a = Number(prompt());
     var b = Number(prompt());
-    console.log(a + b).toFixed(6);
+    console.log(a + b)
 }
 function minus() {
     var a = Number(prompt());
     var b = Number(prompt());
-    console.log(a - b).toFixed(6);
+    console.log(a - b)
 }
 function division() {
     var a = Number(prompt());
     var b = Number(prompt());
-    console.log(a / b).toFixed(6);
+    console.log(a / b)
 }
 function multiply() {
     var a = Number(prompt());
     var b = Number(prompt());
-    console.log(a * b).toFixed(6);
+    console.log(a * b)
 }
