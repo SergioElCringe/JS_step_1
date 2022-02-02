0df750a12c520891283add8b7a4646a69fad72a5
DenisKungurov <denkungurow@yandex.ru>
2022-02-02 20:39:33 +0400

    hw_1

---
+++ students/kungurov_hw_1/script.js
@@ -0,0 +1,15 @@
+var a, b;
+a = Number(prompt());
+b = Number(prompt());
+function plus() {
+    console.log(a + b)
+}
+function minus() {
+    console.log(a - b)
+}
+function division() {
+    console.log(a / b)
+}
+function multiply() {
+    console.log(a * b)
+}
