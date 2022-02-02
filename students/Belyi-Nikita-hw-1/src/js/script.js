5d3197f89d5db74a4c5130503fc43a5fe54e1fd3
Nikita Belyi <whitesiberianperson@gmail.com>
2022-02-02 20:27:55 +0300

    work compleate

---
+++ students/Belyi-Nikita-hw-1/src/js/script.js
@@ -0,0 +1,27 @@
+function sum() {
+let a = +prompt('Enter A');
+let b = +prompt('Enter B');
+let sum = a + b;
+console.log(sum);
+};
+
+function minus() {
+    let a = prompt('Enter A');
+    let b = prompt('Enter B');
+    let min = a - b;
+    console.log(min);
+};
+
+function multiplycation() {
+    let a = prompt('Enter A');
+    let b = prompt('Enter B');
+    let multy = a * b;
+    console.log(multy);
+};
+
+function division() {
+    let a = prompt('Enter A');
+    let b = prompt('Enter B');
+    let divis = a / b;
+    console.log(divis);
+};
