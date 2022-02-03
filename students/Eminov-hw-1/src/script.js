14d2f3a429eb56efa49743059d7119c314802a1b
Rustam <danchoo14@bk.ru>
2022-02-03 13:58:08 +0300

    1: adding a calculator

---
+++ students/Eminov-hw-1/src/script.js
@@ -0,0 +1,27 @@
+function plus() {
+    let firstNumber = +prompt('Enter first number');
+    let secondNumber = +prompt('Enter second number');
+    let sum = firstNumber + secondNumber;
+    console.log(sum);
+};
+
+function minus() {
+    let firstNumber = +prompt('Enter first number');
+    let secondNumber = +prompt('Enter second number');
+    let sum = firstNumber - secondNumber;
+    console.log(sum);
+};  
+
+function multiplication() {
+    let firstNumber = +prompt('Enter first number');
+    let secondNumber = +prompt('Enter second number');
+    let sum = firstNumber * secondNumber;
+    console.log(sum);
+};  
+
+function division() {
+    let firstNumber = +prompt('Enter first number');
+    let secondNumber = +prompt('Enter second number');
+    let sum = firstNumber / secondNumber;
+    console.log(sum);
+};  
