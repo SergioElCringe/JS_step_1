0bf83672a3b7a3e1085a57a27fc44064b39f6689
Andrey <100trof@gmail.com>
2022-02-03 16:52:01 +0300

    trofimov-hw-js-1

---
+++ students/Trofimov-hw-1/script.js
@@ -0,0 +1,31 @@
+function plus() {
+    let num1 = Number(prompt("Введите 1ое число:"));
+    let num2 = Number(prompt("Введите 2ое число:"));
+    let result = num1 + num2;
+
+    console.log(Number(result));
+};
+
+function minus() {
+    let num1 = Number(prompt("Введите 1ое число:"));
+    let num2 = Number(prompt("Введите 2ое число:"));
+    let result = num1 - num2;
+
+    console.log(Number(result));
+};
+
+function mult() {
+    let num1 = Number(prompt("Введите 1ое число:"));
+    let num2 = Number(prompt("Введите 2ое число:"));
+    let result = num1 * num2;
+
+    console.log(Number(result));
+};
+
+function divide() {
+    let num1 = Number(prompt("Введите 1ое число:"));
+    let num2 = Number(prompt("Введите 2ое число:"));
+    let result = num1 / num2;
+
+    console.log(Number(result));
+};
