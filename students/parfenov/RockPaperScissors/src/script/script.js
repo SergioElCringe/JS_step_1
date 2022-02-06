9d11c919d2fa1d22f4e21821eafddd5f642ade75
GeoParf <theoak@list.ru>
2022-02-06 12:18:45 +0300

    parfenov-hw-2-corrected

--- students/parfenov/RockPaperScissors/src/script/script.js
+++ students/parfenov/RockPaperScissors/src/script/script.js
@@ -1,19 +1,19 @@
 
-const rock = 0;
-const paper = 1;
-const scissors = 2;
+// Камень - 1;
+// Бумага = 2;
+// Ножницы - 3;
 const botWin = 'Бот победил!';
 const playerWin = 'Вы победили';
 const deadHeat = 'Ничья';
 
 function getRandom() {
-    return Math.floor(Math.random() * 3)
+    return Math.floor(Math.random() * 3 + 1)
 }
 
 function getChouse() {
     const chouse = +prompt('Введите: 1 - камень, 2 - бумага, 3-ножницы');
     if (chouse === 1 || chouse === 2 || chouse === 3) {
-        return chouse - 1;
+        return chouse;
     } else {
         return console.log('Вы не выбрали нужное значение. попробуйте еще раз')
     }
@@ -24,11 +24,11 @@ function play() {
     const botItem = getRandom();
 
     switch (playerItem) {
-        case rock: {
-            if (botItem === rock) {
+        case 1: {
+            if (botItem === 1) {
                 console.log(`У вас камень у бота камень.  ${deadHeat}`);
             }
-            else if (botItem === paper) {
+            else if (botItem === 2) {
                 console.log(`У вас камень у бота бумага. ${botWin}`);
             }
             else {
@@ -36,11 +36,11 @@ function play() {
             }
             break;
         }
-        case paper: {
-            if (botItem === rock) {
+        case 2: {
+            if (botItem === 1) {
                 console.log(`У вас бумага у бота камень.  ${playerWin}`);
             }
-            else if (botItem === paper) {
+            else if (botItem === 2) {
                 console.log(`У вас бумага у бота бумага. ${deadHeat}`);
             }
             else {
@@ -48,11 +48,11 @@ function play() {
             }
             break;
         }
-        case scissors: {
-            if (botItem === rock) {
+        case 3: {
+            if (botItem === 1) {
                 console.log(`У вас ножницы у бота камень.  ${botWin} `);
             }
-            else if (botItem === paper) {
+            else if (botItem === 2) {
                 console.log(`У вас ножницы у бота бумага. ${playerWin}`);
             }
             else {
