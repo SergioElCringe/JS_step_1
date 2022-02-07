9ce27b92d1f12e2e2c5e76401b415aa55d5915cb
Mikhail <mr.nimostor@mail.ru>
2022-02-07 17:32:29 +0300

    mistakes were fixed

--- students/makarov-hw-2/3-power-function/power-function.js
+++ students/makarov-hw-2/3-power-function/power-function.js
@@ -1,9 +1,9 @@
-function powerFunc(num, power){
-    if(power === 0){
+function powerFunc(num, power) {
+    if (!power) {
         return 1;
     }
-    if(power === 1){
+    if (power === 1) {
         return num;
     }
     return powerFunc(num, power - 1) * num;
-}
+};
