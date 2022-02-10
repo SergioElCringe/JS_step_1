55f2fc52cfb3117bdd58fe65503163639b438bd5
Mikhail <mr.nimostor@mail.ru>
2022-02-10 14:51:50 +0300

    mistakes were fixed

--- students/makarov-hw-3/2-prime-numbers/prime-numbers.js
+++ students/makarov-hw-3/2-prime-numbers/prime-numbers.js
@@ -2,15 +2,13 @@ function primeNumbers() {
     const arr = [];
     for (let i = 2; i <= 100; i++) {
         if (ifPrime(i)) arr.push(i);
-    }
-
-    function ifPrime(num) {
-        if (num <= 1) return false;
-        for (let i = 2; i <= Math.sqrt(num); i++) {
-            if (!(num % i)) return false;
-        }
-        return num;
-    };
-    
+    }    
     return arr;
+};
+function ifPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (!(num % i)) return false;
+    }
+    return num;
 };
