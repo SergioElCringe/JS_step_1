fb84f7a36e68ca66fea2e30dfcdf4b93b6828bd8
DenisKungurov <denkungurow@yandex.ru>
2022-02-13 18:54:44 +0400

    Kungurov_hw_3

--- students/kungurov_hw/Kungurov_hw_3/arrsecond/script.js
+++ students/kungurov_hw/Kungurov_hw_3/arrsecond/script.js
@@ -1,8 +1,20 @@
 let i = 0;
 let b = [];
 function rand() {
-    let c = Math.floor(Math.random() * 101);
-    b[i] = c;
-    i++;
-    console.log(b);
+    do {
+        let c = Math.floor(Math.random() * 100 + 1);
+        let flag = true;
+        for (let i = 2; i < c; i++) {
+            if (c % i == 0) {
+                flag = false;
+                break;
+            };
+        };
+        if (flag === true) {
+            b[i] = c;
+            i++;
+            console.log(b);
+            return b;
+        };
+    } while (!b[i]);
 };
