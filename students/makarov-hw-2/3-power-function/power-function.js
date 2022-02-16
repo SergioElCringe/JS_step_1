ec806276eccb854784ca6a731fdc4164c88db402
Mikhail <mr.nimostor@mail.ru>
2022-02-16 10:56:13 +0300

    excess files were removed

--- students/makarov-hw-2/3-power-function/power-function.js
+++
@@ -1,9 +0,0 @@
-function powerFunc(num, power) {
-    if (!power) {
-        return 1;
-    }
-    if (power === 1) {
-        return num;
-    }
-    return powerFunc(num, power - 1) * num;
-};
--- students/makarov-hw-3/1-even-and-odd/even-and-odd.js
+++
@@ -1,7 +0,0 @@
-function getEven(arr) {
-    return arr.filter(el => !(el % 2));
-};
-
-function getOdd(arr) {
-    return arr.filter(el => !!(el % 2));
-};
