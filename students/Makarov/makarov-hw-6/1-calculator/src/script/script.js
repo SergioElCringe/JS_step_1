1767fe4cf0e4a521a8db7773650be4b8c3f02e49
Mikhail <mr.nimostor@mail.ru>
2022-03-03 17:49:23 +0300

    calculator logic is done

--- students/Makarov/makarov-hw-6/1-calculator/src/script/script.js
+++ students/Makarov/makarov-hw-6/1-calculator/src/script/script.js
@@ -1,85 +1,148 @@
 const calculator = {
+    container: null,
     outputEl: null,
     buttons: null,
-    input: null,
+    data: ['0'],
+    operands: [],
+    names: {},
     result: null,
-    operation: {
-        _data: {
-            num1: null,
-            num2: null
-        },
-        _name: null,
-        compute(num, operationName) {
-            if(!this.data.num1) {
-                this.data = num;
-                this.name = operationName;
-                return '0';
-            } else if (this.data.num1 && !this.data.num2) {
-                this.data = num;
-                const result = this.data.num1[this.name](this.data.num2);
-                this.name = operationName;
-                this.reset();
-                this.data = result;
-                return result;
-            }
-        },
-        
-        get name() {
-            return this._name;
-        },
-        set name(value) {
-            this._name = value;
-        },
-        get data() {
-            return this._data;
-        },
-        set data(value) {
-            if (!this.data.num1 && !this.data.num2) {
-                this.data.num1 = new Decimal(value);
-                console.log(`num1: ${this.data.num1}`);
-            } else if (this.data.num1 && !this.data.num2) {
-                this.data.num2 = new Decimal(value);
-                console.log(`num2: ${this.data.num2}`);
-            }
-        },
-        reset() {
-            this._data.num1 = null;
-            this._data.num2 = null;
-        }
-    },
 
     init() {
-        this.outputEl = document.querySelector('.calc__screen');
+        this.container = document.querySelector('.calc__container');
+        this.render();
+        this.operands = ['+', '-', '*', '/'];
+        this.names = {
+            '+': 'plus',
+            '-': 'minus',
+            '*': 'times',
+            '/': 'div'
+        },
+            this.outputEl = document.querySelector('.calc__screen');
         this.outputEl.value = '0';
         this.buttons = document.querySelector('.calc__keyboard');
         this.handleEvents();
     },
+
     handleEvents() {
         this.buttons.addEventListener('click', this.chooseAction.bind(this));
     },
+
     chooseAction(event) {
         const trigger = event.target;
         if (trigger.classList.contains('calc__btn_symbol')) {
-            this.getInput(trigger);
-            this.displayInput();
+            this.processDigit(trigger);
+        } else if (trigger.classList.contains('calc__btn_point')) {
+            this.processPoint();
         } else if (trigger.classList.contains('calc__btn_operation')) {
-            const result = this.operation.compute(this.input, trigger.dataset.operation);
-            this.outputEl.value = result;
-            this.input = '0';
+            this.processOperand(trigger);
+        } else if (trigger.classList.contains('calc__btn_plusmin')) {
+            this.processPlusMin();
+        } else if (trigger.classList.contains('calc__btn_ce')) {
+            this.clearEntry();
+        } else if (trigger.classList.contains('calc__btn_c')) {
+            this.clearAll();
         } else if (trigger.classList.contains('calc__btn_result')) {
-            
+            this.compute();
         }
+        this.displayInput();
     },
-    getInput(trigger) {
-        if (this.input === null || this.input === '0') {
-            this.input = trigger.dataset.symbol;
-        } else {
-            this.input += trigger.dataset.symbol;
-        }
-    },
+
     displayInput() {
-        this.outputEl.value = this.input;
+        if (this.data[this.data.length - 1] === 'NaN') {
+            this.data = ['0'];
+            this.outputEl.value = 'Result is undefined';
+        } else {
+            this.outputEl.value = this.data.join(' ');
+        }
     },
+
+    compute() {
+        const data = this.data;
+        let result = new Decimal(data.shift());
+        if (this.data.length === 2) {
+            const el1 = data.shift();
+            const el2 = new Decimal(data.shift());
+            result = result[this.names[el1]](el2);
+        }
+        this.data = [result.toString()];
+    },
+    
+    processDigit(trigger) {
+        const digit = trigger.dataset.symbol;
+        const lastEl = this.data[this.data.length - 1];
+        if (this.operands.includes(lastEl)) {
+            this.data.push(digit);
+        } else if (lastEl !== '0' && lastEl !== 'Infinity' && lastEl !== '-Infinity') {
+            this.data[this.data.length - 1] += digit;
+        } else {
+            this.data[this.data.length - 1] = digit;
+        }
+    },
+
+    processPoint() {
+        const lastEl = this.data[this.data.length - 1];
+        if (!isNaN(lastEl) && !lastEl.split('').includes('.') && isFinite(lastEl)) {
+            this.data[this.data.length - 1] += '.';
+        } else if (this.operands.includes(lastEl)) {
+            this.data.push('0.');
+        }
+    },
+
+    processOperand(trigger) {
+        if (this.data.length === 3) {
+            this.compute();
+        }
+        if (this.operands.includes(this.data[this.data.length - 1])) {
+            this.data.pop();
+            this.data.push(trigger.innerHTML);
+        } else {
+            this.data.push(trigger.innerHTML);
+        }
+    },
+
+    processPlusMin() {
+        if (!isNaN(this.data[this.data.length - 1])) {
+            console.log(this.data[this.data.length - 1]);
+            this.data[this.data.length - 1] = ((-1) * this.data[this.data.length - 1]) + '';
+        }
+    },
+
+    clearEntry() {
+        if (!this.operands.includes(this.data[this.data.length - 1])) {
+            this.data[this.data.length - 1] = '0';
+        }
+    },
+
+    clearAll() {
+        this.data = ['0'];
+    },
+
+    render() {
+        this.container.innerHTML = `
+            <input type="text" class="calc__screen" >
+            <div class="calc__keyboard">
+                <button class="calc__btn calc__btn_symbol" data-symbol="7">7</button>
+                <button class="calc__btn calc__btn_symbol" data-symbol="4">4</button>
+                <button class="calc__btn calc__btn_symbol" data-symbol="1">1</button>
+                <button class="calc__btn calc__btn_plusmin">&#177;</button>
+                <button class="calc__btn calc__btn_symbol" data-symbol="8">8</button>
+                <button class="calc__btn calc__btn_symbol" data-symbol="5">5</button>
+                <button class="calc__btn calc__btn_symbol" data-symbol="2">2</button>
+                <button class="calc__btn calc__btn_symbol" data-symbol="0">0</button>
+                <button class="calc__btn calc__btn_symbol" data-symbol="9">9</button>
+                <button class="calc__btn calc__btn_symbol" data-symbol="6">6</button>
+                <button class="calc__btn calc__btn_symbol" data-symbol="3">3</button>
+                <button class="calc__btn calc__btn_point">.</button>
+                <button class="calc__btn calc__btn_operation" data-operation="plus">+</button>
+                <button class="calc__btn calc__btn_operation" data-operation="minus">-</button>
+                <button class="calc__btn calc__btn_operation" data-operation="times">*</button>
+                <button class="calc__btn calc__btn_operation" data-operation="div">/</button>
+                <button class="calc__btn calc__btn_c">C</button>
+                <button class="calc__btn calc__btn_ce">CE</button>
+                <button class="calc__btn calc__btn_high calc__btn_result">=</button>
+            </div>
+        `;
+    }
 
 }
 
