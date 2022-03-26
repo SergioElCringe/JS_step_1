f26fa8ea0bbb97121a11f8df5e499553398b4d69
DanilaTs <cidanila5@yandex.ru>
2022-03-26 20:11:06 +0300

    hw-1

---
+++ students/Tsibizov/hw-1/src/js/script.js
@@ -0,0 +1,24 @@
+function plus() {
+    let a = +prompt('Enter num1');
+    let b = +prompt('Enter num2');
+    let result = (a + b);
+    console.log(result);
+};
+function minus() {
+    let a = +prompt('Enter num1');
+    let b = +prompt('Enter num2');
+    let result = (a - b);
+    console.log(result);
+};
+function times() {
+    let a = +prompt('Enter num1');
+    let b = +prompt('Enter num2');
+    let result = (a * b);
+    console.log(result);
+};
+function divide() {
+    let a = +prompt('Enter num1');
+    let b = +prompt('Enter num2');
+    let result = (a / b);
+    console.log(result);
+};
