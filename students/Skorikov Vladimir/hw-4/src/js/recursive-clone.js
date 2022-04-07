c5f54af67e6911d4398bda4c606d22028fc2315c
Vl007 <skorikov777@yandex.ru>
2022-04-07 18:40:30 +0300

    fixed last comment

--- students/Skorikov Vladimir/hw-4/src/js/recursive-clone.js
+++ students/Skorikov Vladimir/hw-4/src/js/recursive-clone.js
@@ -1,21 +1,30 @@
-      const olga = {
-        name: 'Olga',
-        age: 32,
-        sayHello() {
-          console.log(`${ this.name } greets you!`);
-        },
-        getOlder() {
-          this.age++;
-        },
-        changeName(name) {
-          this.name = name;
-        }
-      };
+const olga = {
+  name: 'Olga',
+  age: 32,
+  sayHello() {
+      console.log(`${this.name} greets you!`);
+  },
+  getOlder() {
+      this.age++;
+  },
+  changeName(name) {
+      this.name = name;
+  },
+  recursive: {
+      name: 'olga2',
+      age: 32,
+  },
+};
 
-      const cloneOlga = {};
-
-      function recursiveClone() {
-        for (let key in olga) {
-          cloneOlga[key] = olga[key];
-        };
-      };
+function recursiveClone() {
+  const olgaClone = {};
+  for (let key in olga) {
+      if (typeof olga[key] === 'object'){
+          olgaCloje[key] = {};
+      }else{
+          olgaClone[key] = olga[key];
+      }
+  };
+  console.log(olgaClone);
+  console.log(olga);
+};
